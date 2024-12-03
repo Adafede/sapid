c90eb6a07df84081038c055535ac3184470f064e
Adriano Rutz <adriano.rutz@hotmail.com>
2024-12-03 17:51:24 +0100

    feat(ui): update plot_concentration and prepare_concentration functions to use new data sources

---
+++ inst/scripts/1_prepare_concentration.R
@@ -0,0 +1,13 @@
+start <- Sys.time()
+
+pkgload::load_all()
+
+message("This program prepares working concentration determination")
+message("Authors: \n", "AR")
+message("Contributors: \n", "...")
+
+prepare_concentration()
+
+end <- Sys.time()
+
+message("Script finished in ", format(end - start))

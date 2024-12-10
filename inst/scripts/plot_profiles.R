d125504acca64e1075ec31c1d821f7dd8127a2e6
Adriano Rutz <adriano.rutz@hotmail.com>
2024-12-10 14:40:17 +0100

    docs: improve plot_profiles.R script for consistency and readability

--- inst/scripts/plot_profiles.R
+++ inst/scripts/plot_profiles.R
@@ -17,31 +17,30 @@ source(file = "paths.R")
 source(file = "params.R")
 source(file = "r/colors.R")
 
-message("... functions \n")
 
-message("... files ... \n")
-files <- analysis_path_04_output |>
-  list.files(pattern = "^profile.*.tsv", full.names = TRUE)
+input = system.file("extdata", "profiles.tsv", package = "sapid")
 
-message("... profile \n")
-filesList <- files |>
-  furrr::future_map(.f = tidytable::fread)
-
-profiles <- filesList |>
-  tidytable::bind_rows()
+profiles <- input |>
+  tidytable::fread() |>
+  # tidytable::mutate(value = tidytable::if_else(
+  #   condition = session == "session_03",
+  #   true = value / 500,
+  #   false = value
+  # )) |> 
+  tidytable::distinct()
 
 n_panelists <- profiles |>
-  tidytable::distinct(ProductName, J) |>
-  tidytable::group_by(ProductName) |>
+  tidytable::distinct(fraction, jury) |>
+  tidytable::group_by(fraction) |>
   tidytable::count()
 
 presence <- profiles |>
-  tidytable::distinct(ProductName, CJ) |>
-  tidytable::group_by(CJ) |>
+  tidytable::distinct(fraction, jury) |>
+  tidytable::group_by(jury) |>
   tidytable::count(sort = TRUE)
 
 consistent_descriptors <- profiles |>
-  tidytable::distinct(J, taste = newName) |>
+  tidytable::distinct(jury, taste = taste_harmonized) |>
   tidytable::group_by(taste) |>
   tidytable::count() |>
   tidytable::filter(n >= MIN_PANELISTS) |>
@@ -49,60 +48,24 @@ consistent_descriptors <- profiles |>
 
 profiles_consistent <- profiles |>
   tidytable::left_join(n_panelists) |>
-  tidytable::select(ProductName, J, descriptors, name, value, newName, n) |>
-  tidytable::rename(taste = newName) |>
+  tidytable::select(fraction, session, jury, taste_original, taste_harmonized, value) |>
+  tidytable::rename(taste = taste_harmonized) |>
   tidytable::filter(taste %in% consistent_descriptors$taste) |>
   # tidytable::filter(value > 0) |>
-  tidytable::group_by(ProductName, taste) |>
-  ## problem due to FIZZ export
-  tidytable::mutate(median = median(value[value <= 10])) |>
-  tidytable::ungroup() |>
-  tidytable::mutate(value = tidytable::if_else(
-    condition = value > 10,
-    true = median,
-    false = value
-  )) |>
-  tidytable::mutate(
-    value = tidytable::if_else(
-      condition = ProductName > 31 &
-        ProductName < 40,
-      true = 50 * value,
-      false = value
-    )
-  ) |>
-  tidytable::mutate(
-    value = tidytable::if_else(
-      condition = ProductName > 63 &
-        ProductName < 72,
-      true = 0.5 * value,
-      false = value
-    )
-  ) |>
-  tidytable::group_by(ProductName, taste) |>
+  tidytable::group_by(fraction, taste) |> 
   tidytable::mutate(median = value |>
     median()) |>
-  ## problem due to FIZZ export
-  # tidytable::mutate(
-  #   value = tidytable::if_else(
-  #     condition = as.numeric(ProductName) >= min_diluted_fraction &
-  #       as.numeric(ProductName) <= max_diluted_fraction,
-  #     true = dilution_factor * value / n,
-  #     false = value / n
-  #   )
-  # ) |>
-  ## diluted
-  tidytable::select(-descriptors, -name, -n) |>
   tidytable::group_by(taste) |>
   tidytable::mutate(sum_taste = value |>
     sum()) |>
   tidytable::arrange(tidytable::desc(sum_taste)) |>
   tidytable::group_by(sum_taste) |>
   tidytable::mutate(group = tidytable::cur_group_id()) |>
-  tidytable::group_by(ProductName) |>
+  tidytable::group_by(fraction) |>
   tidytable::mutate(sum_name = value |>
     sum()) |>
   tidytable::ungroup() |>
-  tidytable::mutate_rowwise(color = paired[[group]], relative = value / sum_name) |>
+  tidytable::mutate_rowwise(color = discrete_rainbow_14[[group]], relative = value / sum_name) |>
   tidytable::ungroup()
 
 profiles_consistent$taste <-
@@ -122,11 +85,11 @@ profiles_consistent$color <-
 test_basis <- ggplot2::ggplot(
   profiles_consistent,
   ggplot2::aes(
-    x = ProductName,
+    x = fraction,
     y = value,
     fill = taste,
     color = taste,
-    group = ProductName
+    group = fraction
   )
 ) +
   ggplot2::geom_violin(ggplot2::aes(fill = taste)) +
@@ -170,9 +133,9 @@ test_zoom
 
 test_2 <- ggplot2::ggplot(
   profiles_consistent |>
-    tidytable::distinct(ProductName, median, taste, color),
+    tidytable::distinct(fraction, median, taste, color),
   ggplot2::aes(
-    x = ProductName,
+    x = fraction,
     y = median,
     fill = taste,
     color = taste,
@@ -198,7 +161,7 @@ test_2 <- ggplot2::ggplot(
   ggplot2::xlab("sample") +
   ggplot2::ylab("Median score") +
   ggbreak::scale_y_cut(
-    breaks = c(35, 760),
+    breaks = c(47, 1360),
     which = c(1, 3),
     scales = c(0, 1)
   )
@@ -207,13 +170,13 @@ test_2
 
 test_3 <- ggplot2::ggplot(
   profiles_consistent |>
-    tidytable::distinct(ProductName, median, taste, color) |>
+    tidytable::distinct(fraction, median, taste, color) |>
     tidytable::filter(taste == "MOUTHFILLING" |
       taste == "VOLUME" |
       # taste == "FATTY" |
       taste == "ASTRINGENT"),
   ggplot2::aes(
-    x = ProductName,
+    x = fraction,
     y = median,
     fill = taste,
     color = taste,
@@ -239,7 +202,7 @@ test_3 <- ggplot2::ggplot(
   ggplot2::xlab("sample") +
   ggplot2::ylab("Median score") +
   ggbreak::scale_y_cut(
-    breaks = c(20, 245),
+    breaks = c(17, 535),
     which = c(1, 3),
     scales = c(0, 1)
   )
@@ -248,11 +211,11 @@ test_3
 
 test_4 <- ggplot2::ggplot(
   profiles_consistent |>
-    tidytable::distinct(ProductName, median, taste, color) |>
+    tidytable::distinct(fraction, median, taste, color) |>
     tidytable::filter(taste != "BITTER") |>
     tidytable::filter(taste != "MOUTHFILLING"),
   ggplot2::aes(
-    x = ProductName,
+    x = fraction,
     y = median,
     fill = taste,
     color = taste,
@@ -278,7 +241,7 @@ test_4 <- ggplot2::ggplot(
   ggplot2::xlab("sample") +
   ggplot2::ylab("Median score") +
   ggbreak::scale_y_cut(
-    breaks = c(30, 360),
+    breaks = c(37, 807.5),
     which = c(1, 3),
     scales = c(0, 1)
   )
@@ -287,7 +250,7 @@ test_4
 
 test_5 <- ggplot2::ggplot(
   profiles_consistent |>
-    tidytable::distinct(ProductName, median, taste, color) |>
+    tidytable::distinct(fraction, median, taste, color) |>
     # tidytable::filter(taste == "ASTRINGENT"),
     # tidytable::filter(taste == "VOLUME"),
     # tidytable::filter(taste == "SWEET"),
@@ -295,7 +258,7 @@ test_5 <- ggplot2::ggplot(
   # tidytable::filter(taste == "WOODY"),
   # tidytable::filter(taste == "UMAMI"),
   ggplot2::aes(
-    x = ProductName,
+    x = fraction,
     y = median,
     fill = taste,
     color = taste,

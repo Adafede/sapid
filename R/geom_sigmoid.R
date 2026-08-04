56145a0135e96ed88a4ce748476c4824e9a364b9
Adriano Rutz <adriano.rutz@hotmail.com>
2026-08-04 08:38:29 +0200

    feat: add sigmoid function for generating smoothed line coordinates

--- R/geom_sigmoid.R
+++ R/geom_sigmoid.R
@@ -66,3 +66,50 @@ geom_sigmoid <- function(
     params = list(na.rm = na.rm, smooth = smooth, direction = direction, ...)
   )
 }
+
+#' sigmoid
+#'
+#' Creates a longer dataframe with coordinates for a smoothed line.
+#'
+#' @param x_from start x value
+#' @param x_to end x value
+#' @param y_from start y value
+#' @param y_to end y values
+#' @param n number of point that should be smoothed
+#' @param smooth smooth parameter. Higher means less smoothing
+#' @param direction the character x or y depending on direction of smoothing
+#'
+#' @return a data frame
+#'
+sigmoid <- function(
+  x_from,
+  x_to,
+  y_from,
+  y_to,
+  smooth = 5,
+  n = 100,
+  direction = "x"
+) {
+  if (!direction %in% c("x", "y")) {
+    stop("Only the directions x or y is allowed.")
+  }
+
+  if (direction == "x") {
+    x <- seq(-smooth, smooth, length = n)
+    y <- exp(x) / (exp(x) + 1)
+    out <- data.frame(
+      x = (x + smooth) / (smooth * 2) * (x_to - x_from) + x_from,
+      y = y * (y_to - y_from) + y_from
+    )
+  }
+
+  if (direction == "y") {
+    y <- seq(-smooth, smooth, length = n)
+    x <- exp(y) / (exp(y) + 1)
+    out <- data.frame(
+      y = (y + smooth) / (smooth * 2) * (y_to - y_from) + y_from,
+      x = x * (x_to - x_from) + x_from
+    )
+  }
+  out
+}
